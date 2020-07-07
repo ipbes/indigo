2d079ae49b323848776555f8492df327ec8482e2
Benedict Omare <benaboki@gmail.com>
2020-07-07 14:48:36 +0200

    clean settings

--- indigo/settings/production.py
+++ indigo/settings/production.py
@@ -1,6 +1,3 @@
-import os
-
-# fetch the development settings 
 from .settings import *
 
 # Configure default domain name
@@ -14,14 +11,7 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 # ##### SECURITY CONFIGURATION ############################
-
-# TODO: Make sure, that sensitive information uses https
-# TODO: Evaluate the following settings, before uncommenting them
-# Settings from https://www.codingforentrepreneurs.com/blog/ssltls-settings-for-django/
-# redirects all requests to https
 SECURE_SSL_REDIRECT = True
-# session cookies will only be set, if https is used
-SESSION_COOKIE_SECURE = True
-# how long is a session cookie valid?
+SESSION_COOKIE_SECURE = DEBUG
 SESSION_COOKIE_AGE = 1209600
 CSRF_COOKIE_SECURE = True
