21da11124758f479a0943c5526fa3e03f2b97be5
Benedict Omare <benaboki@gmail.com>
2020-07-07 13:15:23 +0200

    simplify production settings

--- indigo/production.py
+++ indigo/production.py
@@ -1,22 +1,9 @@
 from .settings import *
 
 # Configure default domain name
-ALLOWED_HOSTS = [os.environ['WEBSITE_SITE_NAME'] + '.akn4undocs.ipbes.net', '127.0.0.1'] if 'WEBSITE_SITE_NAME' in os.environ else []
 
-# WhiteNoise configuration
-MIDDLEWARE = [                                                                   
-    'django.middleware.security.SecurityMiddleware',
-# Add whitenoise middleware after the security middleware                             
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',                      
-    'django.middleware.common.CommonMiddleware',                                 
-    'django.middleware.csrf.CsrfViewMiddleware',                                 
-    'django.contrib.auth.middleware.AuthenticationMiddleware',                   
-    'django.contrib.messages.middleware.MessageMiddleware',                      
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',                    
-]
+ALLOWED_HOSTS = ['indigo.akn4undocs.ipbes.net', 'www.indigo.akn4undocs.ipbes.net','127.0.0.1']
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Configure Postgres database
