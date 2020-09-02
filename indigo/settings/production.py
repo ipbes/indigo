10145ebfa99e6cda82def831865f0753cff9c60f
Benedict Omare <benaboki@gmail.com>
2020-09-02 12:29:54 +0200

    allowed hosts

--- indigo/settings/production.py
+++ indigo/settings/production.py
@@ -2,7 +2,7 @@ from .settings import *
 
 # Configure default domain name
 
-ALLOWED_HOSTS = ['10.208.58.26', '127.0.0.1', '172.17.0.2','172.17.0.3']
+ALLOWED_HOSTS = ['10.208.58.26', '127.0.0.1', '172.17.0.2', '172.17.0.3']
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
