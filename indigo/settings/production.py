137669ee03880a05e98fb14ac0862c923f2051a9
Benedict Omare <benaboki@gmail.com>
2020-09-02 12:22:15 +0200

    checks

--- indigo/settings/production.py
+++ indigo/settings/production.py
@@ -2,12 +2,10 @@ from .settings import *
 
 # Configure default domain name
 
-ALLOWED_HOSTS = ['10.208.58.26', '127.0.0.1', '172.17.0.2']
+ALLOWED_HOSTS = ['10.208.58.26', '127.0.0.1', '172.17.0.2','172.17.0.3']
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Configure Postgres database
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-
-
