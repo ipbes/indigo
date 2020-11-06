1255f2f508ff0cbf51b56bcfe5ec545b080dbbb6
IPBES Secretariat <secretariat@ipbes.net>
2020-11-06 09:42:33 +0100

    Update production.py
    AWS bug

--- indigo/settings/production.py
+++ indigo/settings/production.py
@@ -16,4 +16,4 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_FILE_OVERWRITE = False
 
 MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage':
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
