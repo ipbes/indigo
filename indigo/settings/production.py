96ac0ac9f871d0be8e07c07270070a43858058a0
IPBES Secretariat <secretariat@ipbes.net>
2020-11-06 09:38:33 +0100

    Update production.py
    Add AWS settings

--- indigo/settings/production.py
+++ indigo/settings/production.py
@@ -9,3 +9,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Configure Postgres database
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+# AWS settings
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_S3_BUCKET')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_FILE_OVERWRITE = False
+
+MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage':
