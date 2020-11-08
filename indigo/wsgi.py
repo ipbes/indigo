9a5c7b323645547be494cebdddf367928bb50a04
Benedict Omare <benaboki@gmail.com>
2020-11-08 22:54:31 +0100

    add environment conditionality

--- indigo/wsgi.py
+++ indigo/wsgi.py
@@ -6,14 +6,14 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-#if os.environ.get('DJANGO_ENV') == 'production':
-#    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'indigo.settings.production')
-#else:
-#    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'indigo.settings.settings')
+if os.environ.get('DJANGO_ENV') == 'production':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'indigo.settings.production')
+else:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'indigo.settings.settings')
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "indigo.settings.production")
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "indigo.settings.production")
 
-from django.core.wsgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application   
 from whitenoise.django import DjangoWhiteNoise
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
