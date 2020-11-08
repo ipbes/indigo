bff8d0c2686b9699cb29c7ffe5a4097e8f00e04e
Benedict Omare <benaboki@gmail.com>
2020-11-08 22:29:54 +0100

    remove staticfiles in production

--- indigo/settings/production.py
+++ indigo/settings/production.py
@@ -4,7 +4,7 @@ from .settings import *
 
 ALLOWED_HOSTS = ['10.208.58.26', '127.0.0.1', '172.17.0.1', '172.17.0.2', '172.17.0.3', '172.17.0.4', '172.17.0.5', '3.238.71.183', 'cbd.indigo.ipbes.net', 'akn.indigo.ipbes.net']
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') #Already in settings.py
 
 # Configure Postgres database
 db_from_env = dj_database_url.config(conn_max_age=500)
