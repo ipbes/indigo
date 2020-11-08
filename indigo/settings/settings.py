0d2119f5151d3a3f39aac11be9e7a3f580f35f15
Benedict Omare <benaboki@gmail.com>
2020-11-08 23:59:28 +0100

    s3 AWS settings

--- indigo/settings/settings.py
+++ indigo/settings/settings.py
@@ -182,10 +182,10 @@ TEMPLATES = [
 if not DEBUG:
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     AWS_S3_FILE_OVERWRITE = False
-    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    #AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    #AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_S3_BUCKET')
-    AWS_S3_REGION_NAME = 'eu-west-1'
+    #AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_DEFAULT_ACL = None
     AWS_S3_OBJECT_PARAMETERS = {
         'CacheControl': 'max-age=86400',
