e5304b77e27ca10a055d7c945419af6148542a38
Benedict Omare <benaboki@gmail.com>
2020-11-08 22:24:02 +0100

    settings error logs

--- indigo/settings/settings.py
+++ indigo/settings/settings.py
@@ -397,13 +397,23 @@ MESSAGE_TAGS = {
 # Logging
 LOGGING = {
     'version': 1,
-    'disable_existing_loggers': True,
+    'disable_existing_loggers': False,
     'formatters': {
+         'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
         'simple': {
             'format': '%(asctime)s %(levelname)s %(module)s %(process)d %(thread)d %(message)s'
-        }
+        },
     },
     'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'indigo.log',
+            'formatter': 'verbose'
+        },
         'console': {
             'level': 'DEBUG',
             'class': 'logging.StreamHandler',
